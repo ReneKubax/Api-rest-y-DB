2f48ace406c5e7d51074ee28c56372af8f7f1aa4
ReneKubax <97212849+ReneKubax@users.noreply.github.com>
2022-08-01 00:05:57 -0600

    update 2

--- api.js
+++ api.js
@@ -4,14 +4,20 @@ const user = require('./user.controller')
 const app = express()
 const port = 3000
 
-app.use(express.json)
+app.use(express.json())
 mongoose.connect('mongodb+srv://ReneKubax:barcelona123@cluster0.cyhw72u.mongodb.net/?retryWrites=true&w=majority')
-app.get('/', user.list)
-app.post('/', user.create)
-app.get('/:id', user.get)
-app.put('/:id', user.update)
-app.patch('/:id', user.update)
-app.delete('/:id', user.destroy)
+app.get('/users', user.list)
+app.post('/users', user.create)
+app.get('/users', user.get)
+app.put('/users', user.update)
+app.patch('/users', user.update)
+app.delete('/users', user.destroy)
+app.use(express.static('app'))
+
+app.get('/', (req, res) =>{
+    console.log(__dirname)
+    res.sendFile(`${__dirname}/index.html`)
+})
 
 app.get('*', (req,res) => {
     res.status(404).send('Esta pagina no existe uwu')
